74850333307a609976698bbfa793903e063802a0
lety <tkrhktyd@gmail.com>
2023-04-23 21:41:54 +0900

    쿼리메소드(Or, Less than, Desc)적용Test

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -49,12 +49,33 @@ class ItemRepositoryTest {
     }
 
     @Test
-    @DisplayName("상품명 조회 테스트")
-    public void findByItemNmTest(){
+    @DisplayName("상품명, 상품상세설명 or 테스트")
+    public void findByItemNmOrItemDetailTest(){
         this.createItemList();
-        List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
         for(Item item : itemList){
             System.out.println(item.toString());
         }
     }
-                            }
+
+    @Test
+    @DisplayName("가격 LessThan 테스트")
+    public void findByPriceLessThanTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThan(10005);
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 내림차순 조회 테스트")
+    public void findByPriceLessThanOrderByPriceDesc(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+}
