0e77c8aedd51ca094616932ab7bfc72e5c1ada57
lety <tkrhktyd@gmail.com>
2023-04-25 22:36:19 +0900

    권한부여

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -33,6 +34,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
           .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
           .logoutSuccessUrl("/")
         ;
+
+    http.authorizeRequests()
+            .mvcMatchers("/", "/members/**", "/item/**", "images/**").permitAll()
+            .mvcMatchers("/admin/**").hasRole("ADMIN")
+            .anyRequest().authenticated();
+
+    http.exceptionHandling()
+            .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception{
+      web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
   }
 
     @Bean
